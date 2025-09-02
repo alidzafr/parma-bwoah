eeb3eaa03bdad80761d558e655b6ec39ef2edaef
alidzafr <alidzafathur@gmail.com>
2025-09-02 12:31:42 +0700

    (checkpoin) routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return 'ini index produk';
     }
 
     /**
@@ -21,7 +21,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return 'ini create produk';
     }
 
     /**
