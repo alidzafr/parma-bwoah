7f3d2fef0fd0efed880be99d169e99dd30af2b08
alidzafr <alidzafathur@gmail.com>
2025-09-12 11:49:51 +0700

    Mobile Front End

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
+Route::get('/detail/{product:slug}', [FrontController::class, 'detail'])->name('front.product.detail');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,6 +20,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('cart', CartController::class)->middleware('role:buyer');
 
     Route::resource('order', InvoiceController::class)->middleware('role:owner|buyer');
 
