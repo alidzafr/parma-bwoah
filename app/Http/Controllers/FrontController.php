5fdf1c60101a6c9a94c6ff000a39cd501e0ed4ed
alidzafr <alidzafathur@gmail.com>
2025-09-22 14:07:37 +0700

    Front Search

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,20 +5,36 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::with('category')->orderBy('id', 'DESC')->take(6)->get();
         $categories = Category::all();
 
         return view('front.index', [
             'products' => $products,
             'categories' => $categories
-        ]); 
+        ]);
     }
 
-    public function detail(Product $product) {
+    public function detail(Product $product)
+    {
         return view('front.detail', ['product' => $product]);
+    }
+
+    public function search(Request $request)
+    {
+        $keyword = $request->input('keyword');
+
+        $products = Product::where('name', 'LIKE', '%' . $keyword . '%')->get();
+
+        // dd($products);
+        return view('front.search', [
+            'products' => $products,
+            'keyword' => $keyword
+        ]);
     }
 }
